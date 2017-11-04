15f4582458c4eb7a9555597f70487ce8dffc80cd
sungjunyoung <wnsdud1861@gmail.com>
2017-11-04 18:19:30 +0900

    presentation, projects 추가

---
+++ js/about.js
@@ -0,0 +1,33 @@
+$(document).ready(function () {
+
+    $('.grid').masonry({
+        itemSelector: '.grid-item',
+        columnWidth: 10
+    });
+
+    $('.grid-item').mouseenter(function () {
+
+
+            $(this).find('.grid-term').css("color", "white");
+            $(this).find('.grid-term').css("text-shadow", "-1px 0 #888, 0 1px #888, 1px 0 #888, 0 -1px #888");
+            $(this).find('.grid-title').css("color", "white");
+            $(this).find('.grid-title').css("text-shadow", "-1px 0 black, 0 1px black, 1px 0 black, 0 -1px black")
+            $(this).css('cursor', 'pointer');
+        }
+    );
+
+
+    $('.grid-item').mouseleave(function () {
+            $(this).find('.grid-term').css("color", "#aaa");
+            $(this).find('.grid-term').css("text-shadow", "");
+            $(this).find('.grid-title').css("color", "#272727");
+            $(this).find('.grid-title').css("text-shadow", "")
+
+        }
+    );
+
+    $('.grid-item').click(function () {
+        const url = $(this).attr('id');
+        window.open(url, '_blank');
+    })
+});
